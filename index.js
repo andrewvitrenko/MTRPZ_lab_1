e4ac25300069bb8ee2bb42f276b204f6e5fbe2b1
andrewvitrenko <avitrenko2004@gmail.com>
2023-02-22 23:34:17 +0100

    feat: created function for parsing coefficients from file

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ const interactiveMode = async () => {
   console.log('interactive');
 };
 
+const getCoefficients = (str) => {
+  const numArray = str.split(' ');
+
+  if (numArray.length !== 3) {
+    return;
+  }
+
+  const isAllNumbers = numArray.every(num => !Number.isNaN(+num));
+
+  if (!isAllNumbers) {
+    return;
+  }
+
+  return {
+    a: +numArray[0],
+    b: +numArray[1],
+    c: +numArray[2],
+  };
+};
+
 const nonInteractiveMode = async () => {
   console.log('non-interactive');
 };
